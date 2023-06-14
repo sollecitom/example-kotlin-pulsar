84e0c2edac6310c23ded2b551527d0c4e7b54728
michele <michele.sollecito@gmail.com>
2023-06-14 11:18:57 +0100

    WIP

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -1,3 +1,12 @@
 dependencies {
+    implementation(platform(libs.kotlinx.coroutines.bom))
 
+
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(platform(libs.strikt.bom))
+    testImplementation(libs.strikt.core)
+    testImplementation(libs.strikt.jvm)
+    testImplementation(libs.strikt.mockk)
+
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
---
+++ code/src/test/kotlin/sollecitom/examples/kotlin/pulsar/code/PulsarExampleTests.kt
@@ -0,0 +1,21 @@
+package sollecitom.examples.kotlin.pulsar.code
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import strikt.api.expectThat
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
+
+@TestInstance(PER_CLASS)
+private class PulsarExampleTests {
+
+    @Test
+    fun `something with Pulsar`() {
+
+        expectThat(true) {
+            isTrue()
+            isFalse()
+        }
+    }
+}
